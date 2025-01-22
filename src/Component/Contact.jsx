2dd760d27c81c5911885aef6ed99121d278c3f17
Pavi89361 <pavithraarusamycs@gmail.com>
2025-01-22 16:27:06 +0530

    two

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -21,9 +21,9 @@ export default function Contact() {
                         </address>
                     </div>
                 </div>
-                <div>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3910.3104630081248!2d77.43322857776317!3d11.457502259691468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba93dbd724d5e27%3A0x19f5240fd685595e!2sVaasthu%20Nagar%2C%20Gobichettipalayam%2C%20Tamil%20Nadu%20638452!5e0!3m2!1sen!2sin!4v1737540866030!5m2!1sen!2sin" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
+                {/* <div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3910.3104630081248!2d77.43322857776317!3d11.457502259691468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba93dbd724d5e27%3A0x19f5240fd685595e!2sVaasthu%20Nagar%2C%20Gobichettipalayam%2C%20Tamil%20Nadu%20638452!5e0!3m2!1sen!2sin!4v1737540866030!5m2!1sen!2sin" width="600" height="450"></iframe>
+                </div> */}
 
             </div>
         </div>
